bf04db033c880c357d63701405d1415535d776c4
Owen Davies <owen@odavi.es>
2021-01-25 20:35:09 +0000

    Add Day10 part 2

--- src/main/scala/advent2017/Day10.scala
+++ src/main/scala/advent2017/Day10.scala
@@ -7,25 +7,50 @@ object Day10 {
 
     def main(args: Array[String]) {
         val lengths = Problem.parseInputLineToList("day10", ",").map(_.toInt)
+        val str = Problem.parseInputToString("day10")
         println(part1(0, 255, lengths))
+        println(getKnotHash(0, 255, str))
     }
 
     def part1(start: Int, end: Int, lengths: List[Int]) = {
-        val out = runHashes(start, end, lengths)
-        out.queue.take(2).product
+        val init = CircularList(end + 1 - start)((start to end):_*)
+        runHashes(init, 0, 0, lengths)._1.queue.take(2).product
     }
 
-    def runHashes(start: Int, end: Int, lengths: List[Int]) =
-        lengths.zipWithIndex.foldLeft(
-            (CircularList(end + 1 - start)((start to end):_*), 0)
+    def getKnotHash(start: Int, end: Int, lengths: String) = {
+        val asciiLengths = lengths.map(_.toInt).toList ++
+                           List(17, 31, 73, 47, 23)
+
+        val sparse = (1 to 64).foldLeft(
+            (CircularList(end + 1 - start)((start to end):_*), 0, 0)
         )(
+            (acc, _) => {
+                val (cl, pos, skip) = acc
+                runHashes(cl, pos, skip % end, asciiLengths)
+            }
+        )._1.queue.toList
+
+        sparse.grouped(16).map(_.reduce((a, b) => a ^ b))
+              .map(_.toHexString).mkString
+    }
+
+    def runHashes
+        ( l: CircularList[Int]
+        , pos: Int
+        , skip: Int
+        , lengths: List[Int]) =
+
+        (lengths zip (skip to skip + lengths.size)).foldLeft((l, pos, skip))(
             (acc, next) => {
                 val (length, skip) = next
-                val (cl, curPos) = acc
-                (hash(cl, curPos, length),
-                 (curPos + length + skip) % (end + 1 - start))
+                val (cl, curPos, _) = acc
+                (
+                    hash(cl, curPos, length),
+                    (curPos + length + skip) % l.size,
+                    skip
+                )
             }
-        )._1
+        )
 
     def hash(cl: CircularList[Int], position: Int, length: Int) =
         cl.reverseSection(position, position + length)
--- src/test/scala/Day10.scala
+++ src/test/scala/Day10.scala
@@ -1,5 +1,7 @@
 package advent2017
 
+import scalaadventutils.CircularList
+
 import org.scalatest.funsuite.AnyFunSuite
 import scala.collection.mutable.Queue
 
@@ -7,13 +9,22 @@ class Day10Spec extends AnyFunSuite {
 
     test("Day 10: part1") {
         val lengths = List(3, 4, 1, 5)
+        val cl = CircularList(5)(0, 1, 2, 3, 4)
 
         assertResult(Queue(3, 4, 2, 1, 0)) {
-            Day10.runHashes(0, 4, lengths).queue
+            Day10.runHashes(cl, 0, 0, lengths)._1.queue
         }
 
         assertResult(12) {
             Day10.part1(0, 4, lengths)
+        }
+    }
+
+    test("Day 10: getKnotHash") {
+        var lengths = "test"
+
+        assertResult("309ae6e8f3cb4acbfcdd3c3f30fcf3") {
+            Day10.getKnotHash(0, 255, lengths)
         }
     }
 }
